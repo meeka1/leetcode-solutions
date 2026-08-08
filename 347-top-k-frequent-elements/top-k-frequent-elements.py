0ff2812573219b667fb25231fe62cf375d368454
Mashkhura Eshmatova <eshmatovamashkhura@gmail.com>
2026-08-09 00:37:32 +0100

    Time: 7 ms (51.92%) | Memory: 22.9 MB (60.24%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,21 +1,17 @@
 import heapq
+from collections import Counter
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         counter = {}
-        for num in nums:
-            if num not in counter.keys():
-                counter[num] = 1
-            else:
-                counter[num] += 1
+        # for num in nums:
+        #     if num not in counter.keys():
+        #         counter[num] = 1
+        #     else:
+        #         counter[num] += 1
+        counter = Counter(nums)
+        
 
-        """
-        counter = {
-            1: 3,
-            2: 2,
-            3: 1
-        }
-        """
         
         pq = [(-value, key) for key, value in counter.items()]
         pq = sorted(pq)
